1820090749c8a86649f4106412680ea7f41921a5
Laury PRIN <prinlauryy@gmail.com>
2026-05-05 15:51:52 +0200

    Ajout test unitaires CustomerTest

---
+++ app/src/test/java/com/kirabium/relayance/CustomerTest.kt
@@ -0,0 +1,41 @@
+package com.kirabium.relayance
+
+import com.kirabium.relayance.domain.model.Customer
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.util.Calendar
+
+
+class CustomerTest {
+
+    @Test
+    fun isNewCustomer_whenCreatedLessThanThreeMonthsAgo_shouldReturnTrue() {
+        // GIVEN
+        val today = Calendar.getInstance()
+        val customer = Customer(1, "Alice Wonderland", "alice@example.com", today.time)
+
+        // WHEN
+        val result = customer.isNewCustomer()
+
+        // THEN
+        assertTrue(result)
+    }
+
+    @Test
+    fun isNewCustomer_whenCreatedMoreThanThreeMonthsAgo_shouldReturnFalse() {
+        // GIVEN
+        val sixMonthsAgo = Calendar.getInstance().apply {
+            add(Calendar.MONTH, -6)
+        }
+        val customer = Customer(1, "Alice Wonderland", "alice@example.com", sixMonthsAgo.time)
+
+        // WHEN
+        val result = customer.isNewCustomer()
+
+        // THEN
+        assertFalse(result)
+    }
+
+
+}
